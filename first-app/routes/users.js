3f5efe93e06cc133d10b1e61c6e6f8a970ea59de
qwq <qwq>
2018-12-23 14:54:42 -0500

    build(*): change the text for index and users.

--- first-app/routes/users.js
+++ first-app/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Xiao Ming, Xiao Li, Lao Wang');
 });
 
 module.exports = router;
