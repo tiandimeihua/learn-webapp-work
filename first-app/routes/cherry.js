1e34e83a8b8c69ae549dfc6b31d74234bb6c3523
qwq <qwq>
2019-04-29 22:51:15 -0400

    fix(*): fix ignore folders.

--- first-app/routes/cherry.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET 611 page. */
-router.get('/', function(req, res, next) {
-  res.render('cherry', { title: 'my cherry' });
-});
-
-module.exports = router;
