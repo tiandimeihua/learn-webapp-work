3f07171f91bee9abeeb6567da317d40ae9d6829a
QAQ <i@Q-Q.com>
2019-05-01 01:06:14 -0400

    docs(*): add the version number for  sample-5.

--- first-app-sample-5-v1/app.js
+++ first-app-sample-5-v1/app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require('cors');
 
 var cherryRouter = require("./routes/cherry");
 var indexRouter = require("./routes/index");
@@ -22,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 app.use("/", cherryRouter);
 app.use("/index", indexRouter);
